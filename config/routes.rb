db103842a8805de49b494a9115a7a8e169826e55
Andrew Eames <ajerxs@gmail.com>
2021-08-24 17:21:20 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, only: [:new, :create, :show, :edit, :update]
+  patch 'artists/:id', to: 'artists#update'
+  resources :genres, only: [:new, :create, :show, :edit, :update]
+  patch 'genres/:id', to: 'genres#update'
+  resources :songs, only: [:index, :new, :create, :show, :edit, :update]
+  patch 'songs/:id', to: 'songs#update'
 end
